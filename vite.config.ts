65d6376104f5a52329e3f244fb67affd101ed585
Wan Afiq <wannfq@users.noreply.github.com>
2025-12-21 21:38:44 +0800

    Set up netlify deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import { nitro } from 'nitro/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
   plugins: [
@@ -17,6 +18,7 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    netlify()
   ],
 })
 
